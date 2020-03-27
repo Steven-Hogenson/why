c5065b12576d40b36709ec0b8144e4a4b9651dd3
Steven Hogenson <33229415+Steven-Hogenson@users.noreply.github.com>
2020-03-27 14:37:56 -0400

    Create ArrayUtilitiesMain.java

---
+++ ArrayUtilitiesMain.java
@@ -0,0 +1,13 @@
+public class ArrayUtilitiesMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        double[] inputArray = {75.8, 26.1, 43.5, 13.0, 28.8, 17.7, 32.2, 35.3, 155.4, 43.8, 371.1, 23.2, 28.2234};
+        ArrayUtilities au = new ArrayUtilities(inputArray);
+        au.printOutput();
+        
+        }
+}
