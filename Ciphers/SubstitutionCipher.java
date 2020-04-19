729908517bc3b0e7f7061c7276701db9a734a0ae
Steven Hogenson <33229415+Steven-Hogenson@users.noreply.github.com>
2020-04-19 14:19:33 -0400

    Update SubstitutionCipher.java

--- Ciphers/SubstitutionCipher.java
+++ Ciphers/SubstitutionCipher.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-
 /**
  *
  * @author Steven Hogenson
@@ -24,6 +23,7 @@ public class SubstitutionCipher implements StringEncoder, StringDecoder {
             if (Character.isLetter(charArr[i])) {//excludes cases for non-letters
                 if (Character.isLowerCase(charArr[i])) {
                     result += (char) (((int) charArr[i] + shift - 97) % 26 + 97);//97 ASCII value for 'a'; if character is lower case
+
                 } else {
                     result += (char) (((int) charArr[i] + shift - 65) % 26 + 65);//65 ASCII value for 'A'; if character is upper case
                 }
@@ -35,22 +35,34 @@ public class SubstitutionCipher implements StringEncoder, StringDecoder {
         return result;
     }
 
-   
     public String decode(String cipherText) {
+        int temp = 0;
         char charArr[] = cipherText.toCharArray();
         String result = "";
         for (int i = 0; i < charArr.length; i++) {
             if (Character.isLetter(charArr[i])) {//excludes cases for non-letters
                 if (Character.isLowerCase(charArr[i])) {
-                    result += (char) (((int) charArr[i] - shift - 97) % 26 + 97);//97 ASCII value for 'a'; if character is lower case (same as encode but subtract shift value)
+                    temp = (((int) charArr[i] - shift - 97) % 26 + 97);//97 ASCII value for 'a'; if character is lower case (same as encode but subtract shift value)
+                    if (temp < 97) {//keeps ASCII values in the range of a-z
+                        temp += 26;
+                    } else if (temp > 122) {
+                        temp -= 26;
+                    }
+                    result += (char) temp;
                 } else {
-                    result += (char) (((int) charArr[i] - shift - 65) % 26 + 65);//65 ASCII value for 'A'; if character is upper case (same as encode but subtract shift value)
+                    temp = (char) (((int) charArr[i] - shift - 65) % 26 + 65);//65 ASCII value for 'A'; if character is upper case (same as encode but subtract shift value)
+                    if (temp < 65) {//keeps ASCII values in the range of A-Z
+                        temp += 26;
+                    }
+                    else if (temp > 90) {
+                        temp -= 26;
+                    }
+                    result += (char) temp;
                 }
             } else {
                 result += charArr[i];
             }
         }
-
         return result;
     }
 
