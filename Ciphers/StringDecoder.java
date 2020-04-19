cfedf97f2964fd24f4b380ad7022b782f3e55170
Steven Hogenson <33229415+Steven-Hogenson@users.noreply.github.com>
2020-04-19 12:30:58 -0400

    Create StringDecoder.java

---
+++ Ciphers/StringDecoder.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Steven Hogenson
+ */
+public interface StringDecoder {
+    
+    public String decode(String cipherText);
+    
+}
