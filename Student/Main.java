75b1698be7477e4dad5def73d193926916f55e69
Steven Hogenson <33229415+Steven-Hogenson@users.noreply.github.com>
2020-05-07 22:42:15 -0400

    Create Main.java

---
+++ Student/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Steven Hogenson
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    ArrayList<Integer> tempGrades = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter id: ");
+        int id = sc.nextInt();
+        System.out.print("enter first name: ");
+        String fName = sc.next();
+        System.out.print("enter last name: ");
+        String lName = sc.next();
+        System.out.print("enter major: ");
+        String major = sc.next();
+        System.out.print("enter grades: ");
+        String grades = sc.next();
+        
+        String tempString = grades;
+        String result[] = tempString.split(",");//splits the string of scores at each comma
+        for (String s : result) {
+            tempGrades.add(Integer.parseInt(s));
+        }
+        Student student1 = new Student(id, fName, lName, major, tempGrades);
+        System.out.println(student1.toString());
+   }
+}
